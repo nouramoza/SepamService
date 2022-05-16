2002f8e1f7fd9a2e9824ba19ab981f7fd1698948
unknown <noura.mozaffari@gmail.com>
2022-05-16 19:24:52 +0430

    Initial Commit

---
+++ src/main/java/com/sepam/sepam/SepamApplication.java
@@ -0,0 +1,13 @@
+package com.sepam.sepam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sepam/sepam/SepamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sepam.sepam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
